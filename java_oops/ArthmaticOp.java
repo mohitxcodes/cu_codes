fc46643bb3deb3a97aa50372985abcbd8dc2271c
Mohit Singh <mohitsinghx12@gmail.com>
2026-01-16 22:00:58 +0530

    operators

---
+++ java_oops/ArthmaticOp.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ArthmaticOp {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int a = scanner.nextInt();
+        System.out.println("Enter second number: ");
+        int b = scanner.nextInt();
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+    }
+}
---
+++ java_oops/RelationalOperators.java
@@ -0,0 +1,13 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 15;
+        System.out.println(num1 > num2);
+        System.out.println(num2 < num1);
+        System.out.println(num1 >= num2);
+        System.out.println(num1 <= num2);
+        System.out.println(num1 == num2);
+        System.out.println(num1 != num2);
+    }
+
+}
